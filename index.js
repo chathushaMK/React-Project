f5620899a6c9dc9170f7f819c38ae005fcbd9945
M.K.U.CHATHURANGA <chathusha15@gmail.com>
2026-05-29 21:05:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ const { Server } = require('socket.io');
 console.log('Starting server...');
 
 const httpServer = createServer();
-const io = new Server(httpServer, {})
+const io = new Server(httpServer, {});
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
