3add89de02baa3a19462f0ceee42e0ea60a47a45
M.K.U.CHATHURANGA <chathusha15@gmail.com>
2026-05-29 21:01:57 +0530

    new1

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 
 console.log('Starting server...');
+
+const httpServer = createServer();
+const io = new Server(httpServer, {})
